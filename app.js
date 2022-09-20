7f19980e844b419662d164eba1233ea18145ad41
Saurabh Satralkar <saurabh.satralkar7@gmail.com>
2022-09-20 15:17:00 +0200

    Commiting first before pushing

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Ljubljana,slo&appid=930ffa8d23a8c516e10eeebcff59bc9d&units=metric";
+
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      
+    });
+  });
+
+
+  res.send("Server is running");
+});
+
+app.listen(3000,function(){
+  console.log("Server is running on port 3000");
+});
